61b01b50b6c4b270dcc19f963a67cf2184bca7bc
Johann Grabner <jgrabner@business-tuning.at>
2021-01-14 08:28:33 +0100

    SolOOPmitCSharp

---
+++ OOPmitCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPmitCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
