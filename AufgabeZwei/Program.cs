b019395e8c78ed08c0bfbd7e26db6f885ec68161
Johann Grabner <jgrabner@business-tuning.at>
2021-01-14 12:53:17 +0100

    Aufgabe Rechtecksberechung

---
+++ AufgabeZwei/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AufgabeZwei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verkettete Konstruktoren :this(
+            //.net Properties für Laenge und Breite, explizit get{.... , automatic properties
+            Rechteck r1 = new Rechteck(10, 20); //Laenge Breite
+            Rechteck r2 = new Rechteck(10); // Laenge == Breite
+
+            int umfang = r1.GetUmfang();
+            int flaeche = r1.Flaeche; // Get-Property
+            /*
+             * public int Flaeche
+            {
+                get
+                {
+                    return _Laenge * Breite; 
+                }
+
+            }*/
+
+            InfoAusgeben(r1); // Laenge, Breite, Umfang, Flaeche
+            InfoAusgeben(r2);
+        }
+    }
+}
