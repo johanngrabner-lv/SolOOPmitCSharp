4935624567466b070869f45cd77b0d4a639edcad
Johann Grabner <jgrabner@business-tuning.at>
2021-01-14 08:47:53 +0100

    Hello Campus02

--- OOPmitCSharp/Program.cs
+++ OOPmitCSharp/Program.cs
@@ -6,7 +6,7 @@ namespace OOPmitCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Campus02!");
         }
     }
 }
